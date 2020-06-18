3dcc897374cd401d8c34223468f680ef92ff3811
Nelson G <njeanpierre23@gmail.com>
2020-06-17 23:12:51 -0400

    API SalesManago Funciona

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Api\SalesManago;
+//use App\Api\SalesManago;
+use Pixers\SalesManagoAPI\Client;
+use Pixers\SalesManagoAPI\SalesManago;
+
 use Carbon\Carbon;
 
 class HomeController extends Controller
@@ -27,34 +30,36 @@ class HomeController extends Controller
     {
         return view('admin.home');
     }
-
+    /*
     public function test()
     {
         $salesmanago = new SalesManago();
-        // Carbon::createFromDate(2020,5,14); // pasar fecha especifica en Tz Mexico_City -5
-        $today = Carbon::today();// => Fecha de Hoy en Tz Mexico_City -5
-
-        // request API 
+        
+        $dt = Carbon::createFromDate(2020, 6, 16, "America/Caracas");
+        //dd($dt->copy()->startOfDay()->timestamp*1000, $dt->copy()->endOfDay()->timestamp*1000);
+        //return
         $reqContactListCreated =  $salesmanago->getCredentials(); // api_key, secret_key, sha
         $reqContactListCreated["requestTime"] = Carbon::now()->timestamp * 1000;
         $reqContactListCreated["owner"] = 'Auxiliarmkt@iesa.cc';
-        $reqContactListCreated["from"] = $today->copy()->startOfDay()->timestamp * 1000; // hoy a las 12:00: am (inicio del dia)
-        $reqContactListCreated["to"] =  Carbon::now()->timestamp * 1000; // hoy hora actual.
+        $reqContactListCreated["from"] = 1592265600000; // hoy a las 12:00: am (inicio del dia)
+        $reqContactListCreated["to"] =  1592351999000; // hoy hora actual.
         //work 
         //$reqContactListCreated["from"] = $today->copy()->startOfDay();//->format('Y-m-d H:i');
         //$reqContactListCreated["to"] =  Carbon::now();//->format('Y-m-d H:i');
 
+        //return $reqContactListCreated;
         // response API 
         $resContactListCreated = $salesmanago->curlSm("https://app3.salesmanago.pl/api/contact/createdContacts", json_encode($reqContactListCreated));
-        
+        //return sizeof($resContactListCreated["createdContacts"]);
         if(!empty($resContactListCreated) && $resContactListCreated["success"]){
             if(sizeof($resContactListCreated["createdContacts"]) > 0){
                 $contactsIds = array();
                 foreach($resContactListCreated["createdContacts"] as $created){
                     array_push($contactsIds, strtolower($created["email"]));
                 }
-
-                $reqDataContactCreated = $salesmanago->getCredentials();
+                //return $contactsIds;
+                $salesmanago2 = new SalesManago();
+                $reqDataContactCreated = $salesmanago2->getCredentials();
                 $reqDataContactCreated["requestTime"] = Carbon::now()->timestamp * 1000;
                 $reqDataContactCreated["email"] = $contactsIds;//array("valo_379@hotmail.com", "therasmus12041204@gmail.com");//$contactsIds;
                 $reqDataContactCreated["owner"]  = $reqContactListCreated["owner"];
@@ -66,5 +71,29 @@ class HomeController extends Controller
             return 'no hay data disponible';
         }
         return 'error en la consulta';
+    }
+    */
+
+
+    public function test(){
+        $client = new Client('o28qhomp7m09zozm', "https://app3.salesmanago.pl/api/", 'kvi2rweud3qlrov3h7lvwbisf8lhcs47', 'nzl7tyqare1ac1rxoeba0vqf7du7pj6o');
+        // Then - initialize SalesManago Services Locator
+        $salesManago = new SalesManago($client);
+
+        $contactResponse = $salesManago->getContactService()->listRecentlyCreated("Auxiliarmkt@iesa.cc", array(
+            "from" => 1592265600000,
+            "to" => 1592351999000
+        ));
+        
+        $contacts = $contactResponse->createdContacts;
+        $emails = array();
+        foreach($contacts as $contact){
+            array_push($emails, $contact->email);
+        }
+
+        $contactsList = $salesManago->getContactService()->listByEmails("Auxiliarmkt@iesa.cc", array(
+            "email" => $emails
+        )); 
+        dd($contactsList);
     }
 }
